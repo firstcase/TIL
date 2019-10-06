d9dadbad1a94c4e36929e492c25942b487e2b5c5
Eunha Jeong <kikicocoi79@gmail.com>
2019-10-06 17:06:34 +0900

    swift_iAmRich_App

---
+++ swift/I Am Rich2/I Am Rich2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich2
+//
+//  Created by lucie on 29/09/2019.
+//  Copyright © 2019 lucie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
