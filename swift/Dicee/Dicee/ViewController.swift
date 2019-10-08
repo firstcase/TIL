088ceb39e5a6012ed4d4afc599945d07a0de05a2
Eunha Jeong <kikicocoi79@gmail.com>
2019-10-08 23:25:27 +0900

    swift유데미강의_주사위앱만들기

---
+++ swift/Dicee/Dicee/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  Dicee
+//
+//  Created by lucie on 08/10/2019.
+//  Copyright © 2019 lucie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    var randomDiceIndex1: Int = 0
+    var randomDiceIndex2: Int = 0
+
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func rollButtonPressed(_ sender: UIButton) {
+        randomDiceIndex1 = Int.random(in: 0 ... 5)
+        randomDiceIndex2 = Int.random(in: 0 ... 5)
+        
+        print(randomDiceIndex1)
+    }
+    
+
+}
+
