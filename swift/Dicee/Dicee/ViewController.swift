a23d244571b3c133597aaed3e54a9d43e9dac952
Eunha Jeong <kikicocoi79@gmail.com>
2019-10-12 21:51:17 +0900

    swift유데미_주사위앱_fin

--- swift/Dicee/Dicee/ViewController.swift
+++ swift/Dicee/Dicee/ViewController.swift
@@ -9,26 +9,18 @@
 import UIKit
 
 class ViewController: UIViewController {
-    
-    var randomDiceIndex1: Int = 0
-    var randomDiceIndex2: Int = 0
 
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
-    var leftDiceNumber = 1
-    var rightDiceNumber = 5
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-    }
-    
     @IBAction func rollButtonPressed(_ sender: UIButton) {
-        diceImageView1.image = [ #imageLiteral(resourceName: "dice1"), #imageLiteral(resourceName: "dice2"), #imageLiteral(resourceName: "dice3"), #imageLiteral(resourceName: "dice4"), #imageLiteral(resourceName: "dice5"), #imageLiteral(resourceName: "dice6")][leftDiceNumber]
-        diceImageView2.image = [ #imageLiteral(resourceName: "dice1"), #imageLiteral(resourceName: "dice2"), #imageLiteral(resourceName: "dice3"), #imageLiteral(resourceName: "dice4"), #imageLiteral(resourceName: "dice5"), #imageLiteral(resourceName: "dice6")][rightDiceNumber]
         
-        leftDiceNumber = leftDiceNumber + 1
+        //주사위랜덤이미지 노출하기
+        let diceArray = [ #imageLiteral(resourceName: "dice1"), #imageLiteral(resourceName: "dice2"), #imageLiteral(resourceName: "dice3"), #imageLiteral(resourceName: "dice4"), #imageLiteral(resourceName: "dice5"), #imageLiteral(resourceName: "dice6")]
+        
+        diceImageView1.image = diceArray.randomElement()
+        diceImageView2.image = diceArray.randomElement()
+       
     }
     
 
