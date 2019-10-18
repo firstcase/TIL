0064e80900ff8082a6e0983efb9832489c053592
Eunha Jeong <kikicocoi79@gmail.com>
2019-10-18 23:06:35 +0900

    python_flask스터디

---
+++ python/.ipynb_checkpoints/hello-checkpoint.py
@@ -0,0 +1,17 @@
+from flask import Flask, escape
+app = Flask(__name__)
+
+@app.route('/user/<username>')
+def show_user_profile(username):
+    # show the user profile for that user
+    return 'User %s' % escape(username)
+
+@app.route('/post/<int:post_id>')
+def show_post(post_id):
+    # show the post with the given id, the id is an integer
+    return 'Post %d' % post_id
+
+@app.route('/path/<path:subpath>')
+def show_subpath(subpath):
+    # show the subpath after /path/
+    return 'Subpath %s' % escape(subpath)
