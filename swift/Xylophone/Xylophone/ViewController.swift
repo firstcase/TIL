680c2803a7956cdb4f8167101df7a9bc5dfb31d9
Eunha Jeong <kikicocoi79@gmail.com>
2019-10-20 23:25:08 +0900

    swift유데미_자일로폰_사운드연결

---
+++ swift/Xylophone/Xylophone/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 28/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func keyPressed(_ sender: UIButton) {
+        playSound()
+    }
+    
+    func playSound() {
+        let url = Bundle.main.url(forResource: "C", withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
+}
