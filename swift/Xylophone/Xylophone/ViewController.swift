4a8e869c0f5f9e9923ec1592815b073eed0ece9e
Eunha Jeong <kikicocoi79@gmail.com>
2019-10-23 23:59:22 +0900

    swift유데미_자일로폰_sender.currentTitle

--- swift/Xylophone/Xylophone/ViewController.swift
+++ swift/Xylophone/Xylophone/ViewController.swift
@@ -18,6 +18,9 @@ class ViewController: UIViewController {
     }
 
     @IBAction func keyPressed(_ sender: UIButton) {
+        
+        print(sender.currentTitle)
+        
         playSound()
     }
     
