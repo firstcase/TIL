ac91a36d1ff132dc6d89b1c95efd838163792cc3
Eunha Jeong <kikicocoi79@gmail.com>
2019-11-02 21:56:50 +0900

    swift유데미_eggTimer3

--- swift/EggTimer-iOS13-master/EggTimer/ViewController.swift
+++ swift/EggTimer-iOS13-master/EggTimer/ViewController.swift
@@ -10,25 +10,14 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let softTime = 5
-    let mediumTime = 7
-    let hardTime = 12
+    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
     
     @IBAction func HardnessSelected(_ sender: UIButton) {
 
-        let hardness = sender.currentTitle
+        let hardness = sender.currentTitle!
         
-        switch hardness {
-        case "Soft":
-            print(softTime)
-        case "Medium":
-            print(mediumTime)
-        case "Hard":
-            print(hardTime)
-        default:
-            print("Error")
-        }
+        let result =  eggTimes[hardness]!
+        
     }
-    
 
 }
