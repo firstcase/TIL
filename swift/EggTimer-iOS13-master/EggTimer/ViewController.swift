379f1739f4042214cf3df5fea4df0727f08ac01d
Eunha Jeong <kikicocoi79@gmail.com>
2019-11-03 19:55:42 +0900

    swift유데미_eggTimer4

--- swift/EggTimer-iOS13-master/EggTimer/ViewController.swift
+++ swift/EggTimer-iOS13-master/EggTimer/ViewController.swift
@@ -10,14 +10,34 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    let eggTimes = ["Soft": 5, "Medium": 7, "Hard": 12]
+    @IBOutlet weak var titleLabel: UILabel!
+    
+    let eggTimes = ["Soft": 300, "Medium": 420, "Hard": 720]
+    
+    var secondsRemaining = 60
+    
+    var timer = Timer()
     
     @IBAction func HardnessSelected(_ sender: UIButton) {
-
+        
+        timer.invalidate()
+        
         let hardness = sender.currentTitle!
         
-        let result =  eggTimes[hardness]!
+        secondsRemaining =  eggTimes[hardness]!
         
+        timer = Timer.scheduledTimer(timeInterval: 1.0, target: self, selector: #selector(updateTimer), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func updateTimer(){
+        if secondsRemaining > 0 {
+            print("\(secondsRemaining) seconds")
+            secondsRemaining -= 1
+        } else {
+            timer.invalidate()
+            titleLabel.text = "Done!"
+        }
     }
 
 }
