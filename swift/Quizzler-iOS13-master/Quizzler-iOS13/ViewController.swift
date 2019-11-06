137fc69278792f80915a4d4f9dd4347855aba626
Eunha Jeong <kikicocoi79@gmail.com>
2019-11-06 23:10:48 +0900

    swift유데미_Quizzler1

---
+++ swift/Quizzler-iOS13-master/Quizzler-iOS13/ViewController.swift
@@ -0,0 +1,43 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+    
+    let quiz = [
+        "Four + Two ia equal to Six.",
+        "Five - Three is greater than One.",
+        "Three + Eight is less than Ten"
+    ]
+    
+    var questionNumber = 0
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        questionLabel.text = quiz[questionNumber]
+        updateUI()
+    }
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        questionNumber += 1
+        updateUI()
+    }
+    
+    func updateUI() {
+        questionLabel.text = quiz[questionNumber]
+    }
+
+
+}
+
