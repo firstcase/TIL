aa837201085f758df717a209572153b88416a2cb
Eunha Jeong <kikicocoi79@gmail.com>
2019-11-15 22:13:20 +0900

    swift유데미_BMI_3

--- swift/BMI-Calculator-iOS13-master/BMI Calculator/Controllers/ViewController.swift
+++ swift/BMI-Calculator-iOS13-master/BMI Calculator/Controllers/ViewController.swift
@@ -13,6 +13,9 @@ class ViewController: UIViewController {
     @IBOutlet weak var heightLabel: UILabel!
     @IBOutlet weak var weightLabel: UILabel!
     
+    @IBOutlet weak var heightSlider: UISlider!
+    @IBOutlet weak var weightSlider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -29,6 +32,13 @@ class ViewController: UIViewController {
         weightLabel.text = "\(weight)kg"
     }
     
-   
+    @IBAction func calculatePressed(_ sender: UIButton) {
+        let height = heightSlider.value
+        let weight = weightSlider.value
+        
+        let bmi = weight / pow(height, 2)
+    
+    }
+    
 }
 
