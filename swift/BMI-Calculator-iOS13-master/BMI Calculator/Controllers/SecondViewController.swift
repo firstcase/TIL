ec6ed2dc0e06ead31f39a7dc6a647b9dd792584d
Eunha Jeong <kikicocoi79@gmail.com>
2019-11-17 19:48:17 +0900

    swift_유데미_BMI4

---
+++ swift/BMI-Calculator-iOS13-master/BMI Calculator/Controllers/SecondViewController.swift
@@ -0,0 +1,17 @@
+//
+//  SecondViewController.swift
+//  BMI Calculator
+//
+//  Created by lucie on 17/11/2019.
+//  Copyright © 2019 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+}
