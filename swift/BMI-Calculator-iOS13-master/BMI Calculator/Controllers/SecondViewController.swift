a2952956ae0eafddeee072d0ceeb5845ae5609c6
Eunha Jeong <kikicocoi79@gmail.com>
2019-11-18 22:55:12 +0900

    swift_유데미_BMI5

--- swift/BMI-Calculator-iOS13-master/BMI Calculator/Controllers/SecondViewController.swift
+++ swift/BMI-Calculator-iOS13-master/BMI Calculator/Controllers/SecondViewController.swift
@@ -10,8 +10,17 @@ import UIKit
 
 class SecondViewController: UIViewController {
     
+    var bmiValue = "0.0"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        view.backgroundColor = .red
+        
+        let label = UILabel()
+        label.text = bmiValue
+        label.frame = CGRect(x: 0, y: 0, width: 100, height: 50)
+        view.addSubview(label)
     }
     
 }
--- swift/BMI-Calculator-iOS13-master/BMI Calculator/Controllers/ViewController.swift
+++ swift/BMI-Calculator-iOS13-master/BMI Calculator/Controllers/ViewController.swift
@@ -37,6 +37,11 @@ class ViewController: UIViewController {
         let weight = weightSlider.value
         
         let bmi = weight / pow(height, 2)
+        
+        let secondVC = SecondViewController()
+        secondVC.bmiValue = String(format: "%.1f", bmi)
+        
+        self.present(secondVC, animated: true, completion: nil)
     
     }
     
