57ec2e7fb75aa39abf4626d5fa2ec80a9f51d601
Eunha Jeong <kikicocoi79@gmail.com>
2019-12-23 23:52:56 +0900

    swift유데미_Clima2

--- swift/Clima-iOS13-master/Clima/Controller/WeatherViewController.swift
+++ swift/Clima-iOS13-master/Clima/Controller/WeatherViewController.swift
@@ -8,17 +8,23 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController {
+class WeatherViewController: UIViewController, UITextFieldDelegate {
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var cityLabel: UILabel!
+    @IBOutlet weak var searchTextField: UITextField!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        searchTextField.delegate = self
     }
 
-
+    @IBAction func searchPressed(_ sender: UIButton) {
+        print(searchTextField.text!)
+    }
+    
+  
 }
 
