dc4b231b296192c5548637291aa264d8559a3a34
Eunha Jeong <kikicocoi79@gmail.com>
2020-01-01 23:11:48 +0900

    swift유데미_Clima6

--- swift/Clima-iOS13-master/Clima/Model/WeatherManager.swift
+++ swift/Clima-iOS13-master/Clima/Model/WeatherManager.swift
@@ -9,10 +9,28 @@
 import Foundation
 
 struct WeatherManager {
-    let weatherURL = "http://api.openweathermap.org/data/2.5/weather?appid=5dec4d5a187a3f380b396feb7ff1484f&units=metric"
+    let weatherURL = "https://api.openweathermap.org/data/2.5/weather?appid=5dec4d5a187a3f380b396feb7ff1484f&units=metric"
     
     func fetchWeather(cityName: String) {
         let urlString = "\(weatherURL)&q=\(cityName)"
         print(urlString)
+    }
+    
+    func performRequest(urlString: String){
+        if let url = URL(string: urlString) {
+            let session = URLSession(configuration: .default)
+            let task = session.dataTask(with: url, completionHandler: handle(data: response: error: ))
+            task.resume()
+        }
+    }
+    func handle(data: Data?, response: URLResponse?, error: Error?) {
+        if error != nil {
+            print(error!)
+            return
+        }
+        if let safeData = data {
+            let dataString = String(data: safeData, encoding: .utf8)
+            print(dataString)
+        }
     }
 }
